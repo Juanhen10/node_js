a68e8aa86d9f95683c6b1d9cc8980152d1898e06
juan <henriquejuan36@gmail.com>
2023-02-24 10:15:23 -0300

    nodejs

---
+++ testing/calc.js
@@ -0,0 +1,9 @@
+var  Somarfunc = require("./soma");
+var SubFunc = require("./sub");
+var multFunc = require("./multiplicar");
+var DivFunc = require("./dividir");
+
+console.log(Somarfunc(1,2));
+console.log(SubFunc(5,3));
+console.log(multFunc(8,3));
+console.log(DivFunc(3,3));
